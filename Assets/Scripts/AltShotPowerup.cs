1ee2f8426e6e5f26e4a45af75b86b307ef6a119d
JEMilburn7 <jemilburn7@gmail.com>
2025-04-07 15:46:40 -0500

    Created Powerup Prefabs
    Created the prefabs for the powerups along with the programming to call their effects on the ship.

---
+++ Assets/Scripts/AltShotPowerup.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class AltShotPowerup : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag=="Player")
+        {
+            //colision.ShipController.setAlt();
+            collision.gameObject.GetComponent<SpaceshipController>().setAlt();
+        }
+    }
+}
