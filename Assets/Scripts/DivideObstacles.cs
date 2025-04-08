017e853f891f4e26970b308133d85f1e42839424
Dudley10 <cameron.dudley@bruins.belmont.edu>
2025-04-07 21:51:04 -0500

    Added score to game scene
    score is now adding when you destroy asteroids

--- Assets/Scripts/DivideObstacles.cs
+++ Assets/Scripts/DivideObstacles.cs
@@ -81,7 +81,7 @@ public class DivideObstacles : MonoBehaviour
             
                 }
 
-                // GameManager.addScore(baseScore);
+                GameManager.addScore(baseScore);
                 Destroy(gameObject);
 
             }
---
+++ Assets/Scripts/YourScore.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+public class YourScore : MonoBehaviour
+{
+     //static public TMP_Text scoreText;
+//void start(){
+    //GameManager.Instance.score += 10;
+//}
+//static void UpdateUI(){
+//        scoreText.text = "Score " + score.ToString();
+//    }
+}
